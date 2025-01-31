59b518eb480fbbeb553adaedb7d89a03ef3e70d6
Lucas Cardoso <lucascardosopsd@gmail.com>
2025-01-31 14:19:59 -0300

    feat: get history text from cms in landing page

--- src/app/(front)/(main)/page.tsx
+++ src/app/(front)/(main)/page.tsx
@@ -1,14 +1,18 @@
+"use server";
+import { getLandingPageData } from "@/actions/landingPage/getData";
 import HeroSection from "@/components/sections/Hero";
 import HighlightsSection from "@/components/sections/Highlights";
 import HistorySection from "@/components/sections/History";
 import LocationsSection from "@/components/sections/locations";
 import { Separator } from "@/components/ui/separator";
 
-const Main = () => {
+const Main = async () => {
+  const landingPageData = await getLandingPageData();
+
   return (
     <div>
       <HeroSection />
-      <HistorySection />
+      <HistorySection textData={landingPageData.history} />
       <Separator />
       <HighlightsSection />
       <Separator />
--- src/components/sections/History.tsx
+++ src/components/sections/History.tsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
+import { RichText } from "@payloadcms/richtext-lexical/react";
+import { SerializedEditorState } from "@payloadcms/richtext-lexical/lexical";
 
-const HistorySection = () => {
+const HistorySection = ({ textData }: { textData: SerializedEditorState }) => {
   return (
     <section
       className="max-w-6xl flex flex-col tablet:flex-row justify-center items-center w-full h-full flex-1 min-h-svh mx-auto p-10 tablet:px-0 gap-10"
@@ -21,31 +23,7 @@ const HistorySection = () => {
           <p className="text-end">História</p>
         </div>
 
-        <p className="text-justify">
-          O Café Aporé tem suas raízes na tradição e na paixão pelo café.
-          Fundada no início da década de 1960 por famílias tradicionais de
-          Jales-SP, a empresa nasceu com o propósito de levar um café de
-          qualidade superior à mesa dos brasileiros. Com o passar dos anos, a
-          marca se consolidou no mercado e expandiu sua distribuição para mais
-          de 100 cidades, sempre mantendo seu compromisso com a excelência.
-        </p>
-
-        <p className="text-justify">
-          Há quase quatro décadas, o Café Aporé passou a ser administrado por
-          uma nova gestão, que manteve viva a essência artesanal da torrefação,
-          aliando inovação e tecnologia ao processo de produção. Hoje,
-          utilizamos equipamentos de alta tecnologia para garantir um café de
-          sabor inconfundível, preservando os aromas e características que fazem
-          do nosso produto uma referência no segmento.
-        </p>
-
-        <p className="text-justify">
-          Nosso compromisso com a qualidade é certificado pelo selo da
-          Associação Brasileira da Indústria de Café (ABIC), garantindo que cada
-          xícara de Café Aporé ofereça uma experiência única aos consumidores.
-          Com respeito e dedicação, seguimos aprimorando nossos processos para
-          levar o melhor café até você.
-        </p>
+        <RichText data={textData} />
       </div>
     </section>
   );
