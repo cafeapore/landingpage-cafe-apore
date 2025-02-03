b98db0e5057c631be1b18f7ec8a0296f48e066c8
Lucas Cardoso <lucascardosopsd@gmail.com>
2025-02-03 16:51:34 -0300

    update: blog navbar layout

--- src/components/blog/Navbar.tsx
+++ src/components/blog/Navbar.tsx
@@ -8,23 +8,34 @@ const NavbarBlog = () => {
   return (
     <nav className="h-full tablet:h-20 px-5 py-5">
       <div className="max-w-screen-desktop w-full mx-auto flex flex-col tablet:flex-row items-center gap-5 justify-between">
-        <Link href="/blog">
-          <div className="flex items-center gap-1">
-            <p className="-rotate-90">Blog</p>
-            <Separator orientation="vertical" className="h-20 w-1 bg-red-500" />
-            <Image
-              alt="Logo"
-              src="/logo-preto.png"
-              height={200}
-              width={200}
-              className="h-10 w-auto"
-            />
-          </div>
-        </Link>
+        <div className="flex justify-between items-center w-full">
+          <Link href="/blog">
+            <div className="flex items-center gap-1">
+              <p className="-rotate-90">Blog</p>
+              <Separator
+                orientation="vertical"
+                className="h-20 w-1 bg-red-500"
+              />
+              <Image
+                alt="Logo"
+                src="/logo-preto.png"
+                height={200}
+                width={200}
+                className="h-10 w-auto"
+              />
+            </div>
+          </Link>
+
+          <Link href="/" className="block tablet:hidden">
+            <Button className="text-ring font-semibold bg-yellow-500 px-10 w-full tablet:w-min">
+              Site
+            </Button>
+          </Link>
+        </div>
 
         <SearchField />
 
-        <Link href="/">
+        <Link href="/" className="hidden tablet:block">
           <Button className="text-ring font-semibold bg-yellow-500 px-10 w-full tablet:w-min">
             Site
           </Button>
