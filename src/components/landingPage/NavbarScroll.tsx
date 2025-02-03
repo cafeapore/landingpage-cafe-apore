0673599a469b20259a4de513f046ce979c866650
Lucas Cardoso <lucascardosopsd@gmail.com>
2025-02-03 11:28:36 -0300

    fix: navbar scroll errors -> missing ref

--- src/components/landingPage/NavbarScroll.tsx
+++ src/components/landingPage/NavbarScroll.tsx
@@ -1,48 +1,18 @@
 "use client";
-import { Button } from "../ui/button";
 import { useEffect, useRef } from "react";
 import Navbar from "./Navbar";
 
 const NavbarScroll = () => {
-  const menuOptions = [
-    {
-      title: "Início",
-      route: "#hero",
-    },
-    {
-      title: "Sobre",
-      route: "#about",
-    },
-    {
-      title: "Produtos",
-      route: "#products",
-    },
-    {
-      title: "Regiões",
-      route: "#provinces",
-    },
-    {
-      title: "Blog",
-      route: "/blog",
-    },
-  ];
-
-  const contactButton = (
-    <Button className="bg-yellow-500 text-foreground font-bold px-10">
-      Contato
-    </Button>
-  );
-
   const navbarRef = useRef<HTMLDivElement>(null);
 
   const scrollNav = () => {
     var y = window.scrollY;
     if (y >= 400) {
-      navbarRef!.current!.classList.add("opacity-100");
-      navbarRef!.current!.classList.remove("opacity-0");
+      navbarRef?.current?.classList.add("opacity-100");
+      navbarRef?.current?.classList.remove("opacity-0");
     } else {
-      navbarRef!.current!.classList.add("opacity-0");
-      navbarRef!.current!.classList.remove("opacity-100");
+      navbarRef?.current?.classList.add("opacity-0");
+      navbarRef?.current?.classList.remove("opacity-100");
     }
   };
 
