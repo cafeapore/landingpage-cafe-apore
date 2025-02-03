9e26717dc0d7f8464cabd46aeb3be2af9fd2a71f
Lucas Cardoso <lucascardosopsd@gmail.com>
2025-02-03 15:01:17 -0300

    update: hero section layout

--- src/components/landingPage/sections/Hero.tsx
+++ src/components/landingPage/sections/Hero.tsx
@@ -1,25 +1,43 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 const HeroSection = ({ videoUrl }: { videoUrl: string }) => {
   return (
     <section className="w-full bg-ring/75 tablet:bg-ring/100 relative flex items-center justify-center  overflow-hidden min-h-[calc(100svh-5rem)]">
       <div className="tablet:max-w-6xl flex w-full items-center">
-        <div className="px-10 py-5 mx-auto flex flex-col items-center justify-center h-full flex-1 text-background capitalize font-bold">
+        <div className="px-10 gap-2 py-5 mx-auto flex flex-col items-center justify-center h-full flex-1 text-background capitalize font-bold">
           {/* Title */}
-          <p className="text-5xl tablet:text-6xl border-b-2 border-dashed pb-2 text-center tablet:text-end">
+          <p className="text-4xl tablet:text-6xl border-b-2 border-dashed pb-2 text-center tablet:text-end">
             O <span className="text-yellow-500">melhor</span> café da nossa{" "}
             <span className="text-yellow-500">região</span>
           </p>
 
-          <p className="text-2xl tablet:text-4xl text-center tablet:text-end">
+          <p className="text-xl tablet:text-4xl text-center tablet:text-end">
             Do <span className="text-yellow-500">cotidiano</span> ao{" "}
             <span className="text-yellow-500">100%</span> arábico
           </p>
 
-          <p className="font-normal text-center tablet:text-end normal-case">
+          <p className="font-normal  normal-case text-justify tablet:text-end">
             Desde 1962 unindo qualidade e produção, entregando produtos que
             figuram entre os melhores do interior de São Paulo.
           </p>
+
+          <div className="mt-4 flex justify-center tablet:justify-end gap-5 w-full">
+            <Link href="/blog">
+              <Button className="bg-yellow-500 hover:bg-yellow-600 hover:text-ring min-w-32">
+                Notícias
+              </Button>
+            </Link>
+
+            <Button
+              variant="outline"
+              className="text-ring font-semibold min-w-32"
+            >
+              Loja
+            </Button>
+          </div>
         </div>
 
         {/* video mobile */}
