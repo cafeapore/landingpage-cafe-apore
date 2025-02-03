b130707d55296a319dc6b112f41a5051b1a8a157
Lucas Cardoso <lucascardosopsd@gmail.com>
2025-02-03 09:08:29 -0300

    action: get main news

---
+++ src/actions/news/getMainNews.ts
@@ -0,0 +1,17 @@
+"use server";
+
+import { payload } from "@/lib/payload";
+import { MainNews } from "@/types/cms";
+
+export const getMainNewsData = async (): Promise<MainNews> => {
+  return (await payload.findGlobal({
+    slug: "mainNews",
+    select: {
+      id: true,
+      image: true,
+      content: true,
+      description: true,
+      title: true,
+    },
+  })) as MainNews;
+};
