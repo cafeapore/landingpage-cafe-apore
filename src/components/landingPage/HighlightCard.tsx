56857c1c88396b2c8b3c1653ede8711c0a723539
Lucas Cardoso <lucascardosopsd@gmail.com>
2025-02-04 16:04:17 -0300

    update: highlight card layout -> scale & translate effects in desktop

--- src/components/landingPage/HighlightCard.tsx
+++ src/components/landingPage/HighlightCard.tsx
@@ -8,24 +8,26 @@ type HighlightCardProps = {
 
 const HighlightCard = ({ product }: HighlightCardProps) => {
   return (
-    <Card className="bg-ring relative hover:scale-105 transition -z-10">
-      <div className="absolute h-full w-full bg-gradient-to-t from-ring to-transparent rounded-lg" />
+    <Card className="bg-ring group overflow-hidden">
+      <CardContent className="flex flex-col min-h-52 h-full p-5 gap-2 relative">
+        <div className="absolute h-full w-full bg-gradient-to-t from-ring to-transparent rounded-lg z-10" />
 
-      <CardContent className="flex flex-col min-h-52 h-full p-5 gap-2">
         {product.image?.url && (
           <Image
             alt="Imagem do produto"
             src={product.image.url}
             height={1000}
             width={1000}
-            className="h-52 w-full object-contain"
+            className="h-52 w-full object-contain tablet:group-hover:scale-110 transition"
           />
         )}
 
-        <p className="font-bold text-yellow-500 leading-none z-10">
+        <p className="font-bold text-yellow-500 leading-none z-20 tablet:group-hover:translate-y-2 transition text-center">
           {product.name}
         </p>
-        <p className="text-xs text-background z-10">{product.description}</p>
+        <p className="text-xs text-background z-20 tablet:group-hover:translate-y-2 transition text-center">
+          {product.description}
+        </p>
       </CardContent>
     </Card>
   );
