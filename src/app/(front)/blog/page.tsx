be051d8b41ca0c267b46cb640a3198f252599462
Lucas Cardoso <lucascardosopsd@gmail.com>
2025-02-04 09:21:04 -0300

    update: blog layout

--- src/app/(front)/blog/page.tsx
+++ src/app/(front)/blog/page.tsx
@@ -53,7 +53,7 @@ const BlogPage = async ({ searchParams }: BlogPage) => {
   });
 
   return (
-    <div className="min-h-[calc(100svh-10rem)] w-full py-5 tablet:py-0 h-full flex items-center">
+    <div className="min-h-[calc(100svh-10rem)] w-full pb-10 tablet:pb-5 tablet:py-0 h-full flex items-center">
       <div className="max-w-screen-xl mx-auto h-full w-full flex items-center justify-center flex-col gap-5 px-5">
         {!query && (
           <p className="text-center text-2xl font-semibold text-ring">
@@ -61,12 +61,12 @@ const BlogPage = async ({ searchParams }: BlogPage) => {
           </p>
         )}
 
-        <div className="flex flex-col items-start tablet:flex-row gap-5 h-full">
+        <div className="flex flex-col items-start tablet:flex-row gap-5 tablet:gap-10 h-full">
           {!query && <MainNewsCard data={mainNews.docs[0]} />}
 
           <Separator className="block tablet:hidden" />
 
-          <div className="flex-1 space-y-5 tablet:min-h-[300px]">
+          <div className="flex-1 space-y-5 tablet:space-y-2">
             {news.docs.map((data) => (
               <NewsCard data={data} key={data.id} />
             ))}
@@ -74,7 +74,7 @@ const BlogPage = async ({ searchParams }: BlogPage) => {
             <Separator />
 
             {news.docs.length ? (
-              <div className="flex  justify-around tablet:justify-end w-full gap-5">
+              <div className="flex justify-around tablet:justify-end w-full gap-5">
                 <Paginate
                   hasNextPage={news.hasNextPage}
                   hasPrevPage={news.hasPrevPage}
