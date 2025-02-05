b3d9bb8f18c5cfa7db885064f06a1d1674a1b153
Lucas Cardoso <lucascardosopsd@gmail.com>
2025-02-05 10:38:49 -0300

    fix: file name when upload

--- src/app/(payload)/collections/Media.ts
+++ src/app/(payload)/collections/Media.ts
@@ -1,33 +1,10 @@
+import { genSlug } from "@/utils/genSlug";
 import type { CollectionConfig } from "payload";
 
 export const Media: CollectionConfig = {
   slug: "media",
   upload: {
     staticDir: "media",
-    imageSizes: [
-      {
-        name: "thumbnail",
-        width: 400,
-        height: 300,
-        position: "centre",
-      },
-      {
-        name: "card",
-        width: 768,
-        height: 1024,
-        position: "centre",
-      },
-      {
-        name: "tablet",
-        width: 1024,
-        // By specifying `undefined` or leaving a height undefined,
-        // the image will be sized to a certain width,
-        // but it will retain its original aspect ratio
-        // and calculate a height automatically.
-        height: undefined,
-        position: "centre",
-      },
-    ],
     adminThumbnail: "thumbnail",
     mimeTypes: ["image/*"],
   },
@@ -37,4 +14,14 @@ export const Media: CollectionConfig = {
       type: "text",
     },
   ],
+  hooks: {
+    beforeValidate: [
+      ({ data, req }) => {
+        if (req.file) {
+          req.file.name = genSlug(req.file.name);
+        }
+        return data;
+      },
+    ],
+  },
 };
--- src/components/landingPage/HighlightCard.tsx
+++ src/components/landingPage/HighlightCard.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Image from "next/image";
 import { Card, CardContent } from "../ui/card";
 import { ProductsProps } from "@/types/cms";
@@ -7,22 +9,21 @@ type HighlightCardProps = {
 };
 
 const HighlightCard = ({ product }: HighlightCardProps) => {
+  console.log(product);
   return (
     <Card className="bg-ring group overflow-hidden">
       <CardContent className="flex flex-col min-h-52 h-full p-5 gap-2 relative">
         <div className="absolute h-full w-full bg-gradient-to-t from-ring to-transparent rounded-lg z-10" />
-
-        {product.image?.url && (
-          <Image
-            priority
-            alt="Imagem do produto"
-            src={product.image.url}
-            height={1000}
-            width={1000}
-            className="h-52 w-full object-contain tablet:group-hover:scale-110 transition"
-          />
-        )}
-
+        (
+        <Image
+          priority
+          alt="Imagem do produto"
+          src={product.image.url}
+          height={1000}
+          width={1000}
+          className="h-52 w-full object-contain tablet:group-hover:scale-110 transition"
+        />
+        )
         <p className="font-bold text-yellow-500 leading-none z-20 tablet:group-hover:translate-y-2 transition text-center">
           {product.name}
         </p>
