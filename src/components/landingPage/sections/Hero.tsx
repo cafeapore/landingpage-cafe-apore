e79b1948bd84777d3c0bbc1d4ead02bc6a1abef9
Lucas Cardoso <lucascardosopsd@gmail.com>
2025-02-06 10:21:36 -0300

    update: hero text

--- src/components/landingPage/sections/Hero.tsx
+++ src/components/landingPage/sections/Hero.tsx
@@ -9,14 +9,13 @@ const HeroSection = ({ videoUrl }: { videoUrl: string }) => {
       <div className="tablet:max-w-6xl flex w-full items-center">
         <div className="px-10 gap-2 py-5 mx-auto flex flex-col items-center justify-center h-full flex-1 text-background capitalize font-bold">
           {/* Title */}
-          <p className="text-4xl tablet:text-6xl border-b-2 border-dashed pb-2 text-center tablet:text-end">
+          <p className="text-4xl tablet:text-6xl border-b-2 border-dashed pb-5 text-center tablet:text-end">
             O <span className="text-yellow-500">melhor</span> café da nossa{" "}
             <span className="text-yellow-500">região</span>
           </p>
 
           <p className="text-xl tablet:text-4xl text-center tablet:text-end">
-            Do <span className="text-yellow-500">cotidiano</span> ao{" "}
-            <span className="text-yellow-500">100%</span> arábico
+            {/* Sub Título */}
           </p>
 
           <p className="font-normal normal-case text-center tablet:text-end">
