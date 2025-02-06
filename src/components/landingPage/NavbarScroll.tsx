4e81c7ca44be867f35254e2a6923b905b82fac63
Lucas Cardoso <lucascardosopsd@gmail.com>
2025-02-06 11:29:26 -0300

    update: navbar scroll behaviour

--- src/components/landingPage/NavbarScroll.tsx
+++ src/components/landingPage/NavbarScroll.tsx
@@ -1,29 +1,36 @@
 "use client";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import Navbar from "./Navbar";
 
 const NavbarScroll = () => {
   const navbarRef = useRef<HTMLDivElement>(null);
+  const lastScrollY = useRef(0);
+  const [visible, setVisible] = useState(false);
 
   const scrollNav = () => {
-    var y = window.scrollY;
-    if (y >= 400) {
-      navbarRef?.current?.classList.add("opacity-100");
-      navbarRef?.current?.classList.remove("opacity-0");
+    const currentScrollY = window.scrollY;
+
+    if (currentScrollY > lastScrollY.current && currentScrollY > 400) {
+      // Rolando para baixo → Mostrar Navbar
+      setVisible(true);
     } else {
-      navbarRef?.current?.classList.add("opacity-0");
-      navbarRef?.current?.classList.remove("opacity-100");
+      // Rolando para cima → Esconder Navbar
+      setVisible(false);
     }
+
+    lastScrollY.current = currentScrollY;
   };
 
   useEffect(() => {
     window.addEventListener("scroll", scrollNav);
+    return () => window.removeEventListener("scroll", scrollNav);
   }, []);
 
   return (
     <div
-      className="top-0 left-0 fixed w-full transition z-[100]"
-      id="hero"
+      className={`top-0 left-0 fixed w-full transition-all z-[100] ${
+        visible ? "opacity-100 translate-y-0" : "opacity-0 -translate-y-full"
+      }`}
       ref={navbarRef}
     >
       <Navbar />
