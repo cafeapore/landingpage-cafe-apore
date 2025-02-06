c3d7558ac4270ff83fbeef0cb2743ffeeee78a32
Lucas Cardoso <lucascardosopsd@gmail.com>
2025-02-06 10:40:05 -0300

    update: highlight card background

--- src/components/landingPage/HighlightCard.tsx
+++ src/components/landingPage/HighlightCard.tsx
@@ -10,10 +10,9 @@ type HighlightCardProps = {
 
 const HighlightCard = ({ product }: HighlightCardProps) => {
   return (
-    <Card className="bg-ring group overflow-hidden">
+    <Card className="bg-background border-ring group overflow-hidden p-0">
       <CardContent className="flex flex-col min-h-52 h-full p-5 gap-2 relative">
-        <div className="absolute h-full w-full bg-gradient-to-t from-ring to-transparent rounded-lg z-10" />
-        (
+        <div className="absolute h-full w-full bg-gradient-to-t via-transparent from-background to-transparent rounded-lg z-10" />
         <Image
           priority
           alt="Imagem do produto"
@@ -22,11 +21,11 @@ const HighlightCard = ({ product }: HighlightCardProps) => {
           width={1000}
           className="h-52 w-full object-contain tablet:group-hover:scale-110 transition"
         />
-        )
+
         <p className="font-bold text-yellow-500 leading-none z-20 tablet:group-hover:translate-y-2 transition text-center">
           {product.name}
         </p>
-        <p className="text-xs text-background z-20 tablet:group-hover:translate-y-2 transition text-center">
+        <p className="text-xs text-ring z-20 tablet:group-hover:translate-y-2 transition text-center">
           {product.description}
         </p>
       </CardContent>
