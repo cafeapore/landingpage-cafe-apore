b173abf8935d2b5329172a0c2102e08b980de9e3
lucas cardoso <lucascardosopsd@gmail.com>
2025-02-17 09:41:55 -0300

    fix: remove lenis scroll

--- src/app/(front)/(main)/layout.tsx
+++ src/app/(front)/(main)/layout.tsx
@@ -1,31 +1,9 @@
 "use client";
-import revalidateRoute from "@/actions/revalidateRoute";
 import Footer from "@/components/landingPage/Footer";
 import Navbar from "@/components/landingPage/Navbar";
-import Lenis from "@studio-freight/lenis";
-import { ReactNode, useEffect } from "react";
+import { ReactNode } from "react";
 
 const Layout = ({ children }: { children: ReactNode }) => {
-  useEffect(() => {
-    const lenis = new Lenis({
-      duration: 1.2,
-      easing: (t: any) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-    });
-
-    function raf(time: any) {
-      lenis.raf(time);
-      requestAnimationFrame(raf);
-    }
-
-    requestAnimationFrame(raf);
-
-    revalidateRoute({ route: "/" });
-
-    return () => {
-      lenis.destroy();
-    };
-  }, []);
-
   return (
     <>
       <Navbar />
